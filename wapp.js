0b0809c926d43ab144fa813bcbfe3d25fd7c4e92
Soham Mallick <115361215+soham2106@users.noreply.github.com>
2024-10-02 16:47:47 +0530

    Add files via upload

---
+++ wapp.js
@@ -0,0 +1,36 @@
+const weather = {
+    apikey: "285c037e2c52620f47e56f724ed4e967",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=285c037e2c52620f47e56f724ed4e967")
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
+
+        document.querySelector(".place").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".details").innerText = description;
+        document.querySelector(".windspeed").innerText = " Wind Speed: " + speed + "Km/hr";
+        document.querySelector(".humidity").innerText = " Humidity: " + humidity + "%";
+        document.querySelector(".temp").innerText = (temp - 273.15).toFixed(2) + "° C";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".sbar").value);
+    }
+};
+
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    weather.search();
+});
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+
+
